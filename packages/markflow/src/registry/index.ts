4d2cfed0a5e68ab8835456f84d0246c2420f9563
jp-knj <kenji.tomita1996@gmail.com>
2026-01-17 09:56:54 +0900

    refactor(registry): use functional style instead of class syntax
    - Convert Registry class to factory function returning object
    - Add Registry interface for type safety
    - Update pnpm-lock.yaml for markflow workspace dependency

--- packages/markflow/src/registry/index.ts
+++ packages/markflow/src/registry/index.ts
@@ -1,46 +1,36 @@
 import type { ComponentLibrary, ComponentDefinition, DirectiveMapping } from './types';
 
-export class Registry {
-  private components: Map<string, ComponentDefinition> = new Map();
-  private directives: Map<string, DirectiveMapping> = new Map();
-
-  constructor(libraries: ComponentLibrary[]) {
-    for (const lib of libraries) {
-      for (const comp of lib.components) {
-        this.components.set(comp.name, comp);
-      }
-      for (const dir of lib.directiveMappings ?? []) {
-        this.directives.set(dir.directive, dir);
-      }
-    }
-  }
-
-  getComponent(name: string): ComponentDefinition | undefined {
-    return this.components.get(name);
-  }
-
-  getDirectiveMapping(directive: string): DirectiveMapping | undefined {
-    return this.directives.get(directive);
-  }
-
-  getAllComponents(): ComponentDefinition[] {
-    return Array.from(this.components.values());
-  }
-
-  getSupportedDirectives(): string[] {
-    return Array.from(this.directives.keys());
-  }
-
-  toRustConfig() {
-    return {
-      components: this.getAllComponents(),
-      directiveMappings: Array.from(this.directives.values()),
-    };
-  }
+export interface Registry {
+  getComponent(name: string): ComponentDefinition | undefined;
+  getDirectiveMapping(directive: string): DirectiveMapping | undefined;
+  getAllComponents(): ComponentDefinition[];
+  getSupportedDirectives(): string[];
+  toRustConfig(): { components: ComponentDefinition[]; directiveMappings: DirectiveMapping[] };
 }
 
 export function createRegistry(libraries: ComponentLibrary[]): Registry {
-  return new Registry(libraries);
+  const components = new Map<string, ComponentDefinition>();
+  const directives = new Map<string, DirectiveMapping>();
+
+  for (const lib of libraries) {
+    for (const comp of lib.components) {
+      components.set(comp.name, comp);
+    }
+    for (const dir of lib.directiveMappings ?? []) {
+      directives.set(dir.directive, dir);
+    }
+  }
+
+  return {
+    getComponent: (name: string) => components.get(name),
+    getDirectiveMapping: (directive: string) => directives.get(directive),
+    getAllComponents: () => Array.from(components.values()),
+    getSupportedDirectives: () => Array.from(directives.keys()),
+    toRustConfig: () => ({
+      components: Array.from(components.values()),
+      directiveMappings: Array.from(directives.values()),
+    }),
+  };
 }
 
 export * from './types';
